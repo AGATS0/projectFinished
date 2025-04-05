096e6b0689f8513a3e8a42d2f5d1ef5403a4af65
Vladislav <vantyz757@gmail.com>
2025-04-05 13:50:17 +0300

    phpcomposer

---
+++ main.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+require_once 'vendor/autoload.php';
+
+use Vantyz\Php6class2\PizzaStoreOrders;
+use Vantyz\Php6class2\PizzaStore;
+use Vantyz\Php6class2\Pizza;
+use Vantyz\Php6class2\PizzaWithSolyamy;
+use Vantyz\Php6class2\PizzaWithRock;
+use Vantyz\Php6class2\PizzaWithShaurma;
+
+
+$pizza = new PizzaStoreOrders();
+$pizza->orderPizza("Пицца с сыром");
+
+
+
+
+
+
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vantyz/php4',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vantyz/php4' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vantyz/php6class-2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'edd37a9f8b8aed2c86b590468c197876d01c45cf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../vantyz/php6class-2',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
